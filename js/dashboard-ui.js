001ebe9ff5eab96a0cdeb820e8706d2a5f3d4e1b
arcais <xclubcontest@gmail.com>
2017-05-17 15:06:00 +0300

    Removed manage from interface, added logout button

--- js/dashboard-ui.js
+++ js/dashboard-ui.js
@@ -68,6 +68,35 @@
 //     });
 // })(jQuery);
 
+
+
+//**cookie related stuff ahead**
+function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
+function createCookie(name,value,days) {
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime()+(days*24*60*60*1000));
+        var expires = "; expires="+date.toGMTString();
+    }
+    else var expires = "";
+    document.cookie = name+"="+value+expires+"; path=/";
+}
+
+function eraseCookie(name) {
+    createCookie(name,"",-1);
+}
+
+
 (function ($) {
 
     $("#spinnerLoader").addClass("finishedLoading");
@@ -154,4 +183,11 @@ $("#send_reclamatie").on('click', function(){
 
     }
 
+});
+
+$("#logout").click( function(e){
+    e.preventDefault();
+    eraseCookie("sesid");
+    eraseCookie("username");
+    location.reload();
 });
